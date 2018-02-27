acd6a9fbabe0d7f142c4e1f1963d4a5be28e35c8
aarguellez <alex.arguellez05@gmail.com>
2018-02-27 15:21:43 -0800

    enter

--- website/main.js
+++ website/main.js
@@ -1,45 +1,124 @@
-const startStr = prompt('Enter the start')
-const start= parseInt(startStr,10)
+// Take Input Parameters
+getStartInput();
+getEndInput();
+getStepInput();
 
-alert(start)
+function getStartInput() {
+    start = prompt('Enter the start: ')
+    checkStart(start)
+}
 
-const endStr = prompt('Enter the end')
-const end= parseInt(endStr,10)
+function getEndInput() {
+    end = prompt('Enter the end: ')
+    checkEnd(end)
+}
 
-const stepStr = prompt('Enter the step')
-const step = parseInt(stepStr,10)
+function getStepInput() {
+    step = prompt('Enter the step: ')
+    checkStep(step)
+    isStepNegative(step)
+}
 
-var min = start;
-var max = end;
-var increments = step;
-var arr = [];
-for (var i = 0; i <= step; ++i)
-    arr.push(min + increments * i);
+function isStepNegative(param1){
+    if ((parseInt(start) > parseInt(end)) && (parseInt(param1) > 0))
+    {
+        alert('Step must be negative. Please re-enter. ')
+        getStepInput();
+    }
+}
+
+// Qualify Inputs
+function checkStart(param1) {
+    if (isNaN(param1)) {
+        alert('Invalid Input. Please enter a number.')
+        getStartInput();
+    }
+}
+
+function checkEnd(param1) {
+    if (isNaN(param1)) {
+        alert('Invalid Input. Please enter a number.')
+        getEndInput();
+    }
+}
+
+function checkStep(param1) {
+    if (param1 == 0 || isNaN(param1)) {
+        alert('Invalid Input. Please enter a number not including 0.')
+        getStepInput();
+    }
+}
 
 
-// function createNumArray(start, end):[step];
-// alert(arr)
-// {
-//     var arr = [], 
-//         i = step;
- 
-//      while((arr[arr.length] = i) < end) {i++}
-//      return arr;
-//  }
-// alert(arr)
-// var myNumbers = [start,end];
 
-// Array.prototype.range = function(start, end) {
-//     if (!this.length) {
-//         while (end >= start) {
-//             this.push(start++);
-//         }
-//     } else {
-//         throw "You can only call 'range' on an empty array";
-//     }
-//     return this;
-// };
+// Function 1
+// "The first function generates an array of integer based on the three parameters"
 
-// var array = [].range(1, 20);
-const output = document.getElementById("output")
-output.innerHTML = 'The generated array is ' + arr 
+function myArray(start, end, step) {
+    var startCounter = parseInt(start)
+    var stepCounter = parseInt(step)
+    var endCounter = parseInt(end)
+
+    var numberArray = [startCounter]
+
+    if (startCounter < endCounter) {
+        while ((startCounter + stepCounter) <= endCounter) {
+            startCounter += stepCounter;
+            numberArray.push(startCounter);
+        }
+    }
+    else {
+        while (endCounter <= (startCounter + stepCounter)) {
+            startCounter += stepCounter;
+            numberArray.push(startCounter);
+        }
+    }
+
+
+    return numberArray;
+}
+
+// Function 2
+// "The second one computes the sum of all array elements."
+function sumArray(myArray) {
+    sum = 0
+
+    for (var i = 0; i < myArray.length; i++) {
+        sum += myArray[i];
+    }
+
+    return sum;
+}
+
+
+// Function 3
+// "The last one converts the value into an array of binary representation of the integers."
+function binaryArray(myArray) {
+    var binaryArray = []
+
+    for (var i = 0; i < myArray.length; i++) {
+        binaryArray.push(getBinary(myArray[i]));
+    }
+
+    return binaryArray;
+
+}
+
+function getBinary(param1) {
+    var result = param1.toString(2);
+
+    while (Math.floor(result.length % 8) != 0)
+        result = "0" + result;
+
+    return result;
+}
+
+
+const output1 = document.getElementById('result1')
+output1.innerHTML = 'The generated array is ' + myArray(start, end, step)
+
+const output2 = document.getElementById('result2')
+output2.innerHTML = 'The sum is ' + sumArray(myArray(start, end, step))
+
+const output3 = document.getElementById('result3')
+output3.innerHTML = 'The binary of absolute elements is ' + binaryArray(myArray(start, end, step))
