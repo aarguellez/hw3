8607891e6e930463a156aec4657ee2c45bf45e08
aarguellez <alex.arguellez05@gmail.com>
2018-02-27 15:16:19 -0800

    enter

--- website/main.js
+++ website/main.js
@@ -1,14 +1,45 @@
-// const answer = prompt(question);
+const startStr = prompt('Enter the start')
+const start= parseInt(startStr,10)
 
-const p_start = prompt('Enter the start:');
-const p_step = prompt('Enter the step:');
-const p_end = prompt('Enter the end:');
+alert(start)
 
-const start = number(p_start);
-const step = number(p_step);
-const end = number(p_end);
+const endStr = prompt('Enter the end')
+const end= parseInt(endStr,10)
 
-const my_array = []
+const stepStr = prompt('Enter the step')
+const step = parseInt(stepStr,10)
+
+var min = start;
+var max = end;
+var increments = step;
+var arr = [];
+for (var i = 0; i <= step; ++i)
+    arr.push(min + increments * i);
 
 
-// alert(`You answered ${answer}`);
+// function createNumArray(start, end):[step];
+// alert(arr)
+// {
+//     var arr = [], 
+//         i = step;
+ 
+//      while((arr[arr.length] = i) < end) {i++}
+//      return arr;
+//  }
+// alert(arr)
+// var myNumbers = [start,end];
+
+// Array.prototype.range = function(start, end) {
+//     if (!this.length) {
+//         while (end >= start) {
+//             this.push(start++);
+//         }
+//     } else {
+//         throw "You can only call 'range' on an empty array";
+//     }
+//     return this;
+// };
+
+// var array = [].range(1, 20);
+const output = document.getElementById("output")
+output.innerHTML = 'The generated array is ' + arr 
